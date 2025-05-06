9c5684e8b48e47746adb680dd7a9dd3b5a83ea48
Carlos <carlos>
2025-05-06 08:23:31 -0300

    Commit automático em 06/05/2025 08:23:31

---
+++ commitAuto.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git add .
+
+# Verifica se há mudanças para commitar
+if git diff-index --quiet HEAD --; then
+    echo "Nenhuma mudança para commitar."
+    exit 0
+fi
+
+# Faz o commit com uma mensagem padrão ou personalizada
+if [ -z "$1" ]; then
+    git commit -m "Commit automático em $(date +'%d/%m/%Y %H:%M:%S')"
+else
+    git commit -m "$1"
+fi
+
+# Mostra o status após o commit
+git status
